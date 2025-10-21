4e533adc596ebb8654dcb5a50193234d561ef5d8
Minokainduwara <minokainduwara@gmail.com>
2025-10-21 06:20:25 +0530

    Created an API

--- subscription_tracker/app.js
+++ subscription_tracker/app.js
@@ -1,1 +1,14 @@
-console.log("server is running on localhost: 5000");
+import express from "express";
+const app = express();
+
+//Create an API
+app.get("/", (req, res) => {
+    res.send("Welcome to the EOS App");
+})
+
+
+app.listen(3000,() =>{
+    console.log("Listening on port 3000: http://localhost:3000");
+});
+
+export default app;
