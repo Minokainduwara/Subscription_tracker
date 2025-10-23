e6d014f7b3fcf707ffe4c73cad353ef39bc379a5
Minokainduwara <minokainduwara@gmail.com>
2025-10-24 00:29:31 +0530

    user Routes

---
+++ subscription_tracker/routes/user.routes.js
@@ -0,0 +1,16 @@
+import {Router} from 'express';
+
+const userRouter = Router();
+
+//GET /users --> Get all users
+//GET /users/:id --> Get users by id
+
+userRouter.get('/users',(req, res) => res.send({tittle: 'GET all users'}));
+
+userRouter.get('/:id',(req, res) => res.send({tittle: 'GET user details'}));
+
+userRouter.post('/',(req, res) => res.send({tittle: 'CREATE new users'}));
+
+userRouter.put('/:id',(req, res) => res.send({tittle: 'UPDATE user by id'}));
+
+userRouter.delete('/:id',(req, res) => res.send({tittle: 'Delete user by id'}));
