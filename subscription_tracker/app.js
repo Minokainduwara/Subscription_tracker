dd4ab0a699a9de965f4a87f87c5a7b149afdfe28
Minokainduwara <minokainduwara@gmail.com>
2025-10-24 02:13:04 +0530

    Error fixed listening on port 3000 instead of 5500

--- subscription_tracker/app.js
+++ subscription_tracker/app.js
@@ -1,6 +1,17 @@
 import express from "express";
-const app = express();
+
 import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
+const app = express();
+
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
 
 //Create an API
 app.get("/", (req, res) => {
@@ -8,7 +19,7 @@ app.get("/", (req, res) => {
 })
 
 
-app.listen(3000,() =>{
+app.listen(PORT,() =>{
     console.log(`Listening on port : http://localhost:${ PORT }`);
 });
 
